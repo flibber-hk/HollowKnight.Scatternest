a91dc2b6fa0c4be18791f7e9e13fab9f46a58480
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2024-01-22 22:09:38 +0000

    Separate out SSPM into 2 classes

--- Scatternest/Menu/StartSelectionPageManager.cs
+++ Scatternest/Menu/StartSelectionPageManager.cs
@@ -10,7 +10,7 @@ namespace Scatternest.Menu
 {
     internal static class StartSelectionPageManager
     {
-        public static int ICDLHash { get; private set; }
+        public static int ICDLHash { get; internal set; }
 
         internal static Dictionary<MenuPage, StartSelectionPage> Instances { get; private set; } = new();
 
@@ -19,15 +19,10 @@ namespace Scatternest.Menu
             RandomizerMenuAPI.AddStartGameOverride(ConstructMenu, RandoHandleButton);
             MenuChangerMod.OnExitMainMenu += Instances.Clear;
 
-            if (ModHooks.GetMod("ICDL Mod") is Mod) HookICDL();
+            if (ModHooks.GetMod("ICDL Mod") is Mod) StartSelectionPageManager_ICDL.HookICDL();
         }
 
-        internal static void HookICDL()
-        {
-            ICDLMenuAPI.AddStartGameOverride(ConstructMenu, ICDLHandleButton);
-        }
-
-        private static void ConstructMenu(MenuPage landingPage) => Instances[landingPage] = new(landingPage);
+        internal static void ConstructMenu(MenuPage landingPage) => Instances[landingPage] = new(landingPage);
 
         private static bool RandoHandleButton(RandoController rc, MenuPage landingPage, out BaseButton button)
         {
@@ -35,14 +30,26 @@ namespace Scatternest.Menu
             return Instances.TryGetValue(landingPage, out StartSelectionPage instance) && instance.HandleButton(rc.ctx, out button);
         }
 
-        // This needs to be a function rather than a delegate so that the compiler doesn't generate
-        // a field to hold a reference to the delegate
+    }
+
+    /* This needs to be a separate class because in some builds (e.g. on Github), the compiler
+     * generates a nested class to hold references to the delegates and in some builds it does not.
+     * Rather than forcing a particular toolchain, we'll just nip the problem in the bud
+     * and separate out the ICDL-dependent functions into a new class.
+     */
+    internal static class StartSelectionPageManager_ICDL
+    {
+        internal static void HookICDL()
+        {
+            ICDLMenuAPI.AddStartGameOverride(StartSelectionPageManager.ConstructMenu, ICDLHandleButton);
+        }
+
         private static bool ICDLHandleButton(ICDLMenu.StartData data, MenuPage landingPage, out BaseButton button)
         {
-            ICDLHash = data.Hash();
+            StartSelectionPageManager.ICDLHash = data.Hash();
 
             button = null;
-            return Instances.TryGetValue(landingPage, out StartSelectionPage instance) && instance.HandleButton(data.CTX, out button);
+            return StartSelectionPageManager.Instances.TryGetValue(landingPage, out StartSelectionPage instance) && instance.HandleButton(data.CTX, out button);
         }
     }
 }
